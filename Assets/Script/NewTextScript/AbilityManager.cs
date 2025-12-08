67bce3978254001ba5e131e72fd6c1c1794b2a34
Yuki119937 <129066937+Yuki119937@users.noreply.github.com>
2025-12-08 15:32:31 +0800

    优化了变化图像的逻辑 现在观感变好了

--- Assets/Script/NewTextScript/AbilityManager.cs
+++ Assets/Script/NewTextScript/AbilityManager.cs
@@ -5,45 +5,45 @@ using UnityEngine;
 public class AbilityManager : MonoBehaviour
 {
     [Header("子弹时间设置")]
-    public float slowMotionScale = 0.3f; // 慢动作时的倍速 (0.3 表示 30% 速度)
-    public bool isBulletTime = false;    // 当前是否处于子弹时间
+    public float slowMotionScale = 0.3f;
+    public bool isBulletTime = false;
 
     [Header("引用各个能力脚本")]
-    public PlayerDirectionalDash dashScript;    // 1号能力
-    public BlockBuilder buildScript;            // 2号能力
-    public ModifierTool modifierScript;         // 3号能力
-    public DebugDestroyer debugScript;          // 0号调试工具 (通常不受限制，也可加上)
+    public PlayerDirectionalDash dashScript;
+    public BlockBuilder buildScript;
+    public ModifierTool modifierScript;
+    public DebugDestroyer debugScript;
 
     [Header("引用外观脚本")]
-    public RandomAppearance appearanceScript;   // 变身脚本
+    public RandomAppearance appearanceScript;
 
-    // 记录当前激活的是哪个能力，方便取消
-    private int activeAbilityIndex = -1; // -1表示没激活，1=冲刺, 2=建造, 3=附魔
+    // 记录当前激活的是哪个能力 (-1表示没激活)
+    private int activeAbilityIndex = -1;
 
     void Update()
     {
         HandleBulletTime();
 
-        // 只有在子弹时间（或者已经激活了某种能力）的情况下，才允许切换能力
-        // 这里设计为：必须按住 E 或者 开启了 E 模式才能选能力
         if (isBulletTime)
         {
             HandleAbilityInput();
+
+            // --- 新增：在这里检测鼠标点击 ---
+            // 只有在选了能力的情况下，按下鼠标左键，才变身
+            if (activeAbilityIndex != -1 && Input.GetMouseButtonDown(0))
+            {
+                TriggerAppearanceChange();
+            }
         }
         else
         {
-            // 如果退出了子弹时间，是否要强制关闭所有能力？
-            // 通常为了手感，如果玩家正在瞄准（比如按了2正在旋转方块），突然松开E
-            // 建议：要么保持当前能力，要么强制关闭。
-            // 这里我们设定：松开E（退出子弹时间）会强制关闭所有瞄准状态，恢复正常游戏。
             CloseAllAbilities();
         }
     }
 
     void HandleBulletTime()
     {
-        // --- 修改点：现在使用左 Shift 键 (LeftShift) ---
-        // 按住 Shift 进入子弹时间，松开恢复
+        // 按住 LeftShift 进入子弹时间
         if (Input.GetKey(KeyCode.LeftShift))
         {
             isBulletTime = true;
@@ -60,51 +60,34 @@ public class AbilityManager : MonoBehaviour
 
     void HandleAbilityInput()
     {
-        // 监听 1, 2, 3 (以及 0)
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ActivateAbility(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ActivateAbility(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ActivateAbility(3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            // 0号工具通常作为调试，随时可以用，或者也限制在子弹时间
-            ActivateAbility(0);
-        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ActivateAbility(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) ActivateAbility(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) ActivateAbility(3);
+        else if (Input.GetKeyDown(KeyCode.Alpha0)) ActivateAbility(0);
     }
 
     void ActivateAbility(int index)
     {
-        // 1. 先关闭所有正在运行的能力 (互斥逻辑)
         CloseAllAbilities();
-
-        // 2. 根据按键开启对应的能力
         activeAbilityIndex = index;
 
+        // --- 修改点：这里只负责开启瞄准/预览，不再变身 ---
         switch (index)
         {
             case 1:
                 if (dashScript != null) dashScript.TurnOn();
-                TriggerAppearanceChange(); // 触发变身
+                // TriggerAppearanceChange(); // <--- 这一行被删掉了
                 break;
             case 2:
                 if (buildScript != null) buildScript.TurnOn();
-                TriggerAppearanceChange(); // 触发变身
+                // TriggerAppearanceChange(); // <--- 这一行被删掉了
                 break;
             case 3:
                 if (modifierScript != null) modifierScript.TurnOn();
-                TriggerAppearanceChange(); // 触发变身
+                // TriggerAppearanceChange(); // <--- 这一行被删掉了
                 break;
             case 0:
                 if (debugScript != null) debugScript.TurnOn();
-                // 0号通常不需要变身，或者你想变也可以加
                 break;
         }
     }
@@ -121,10 +104,9 @@ public class AbilityManager : MonoBehaviour
 
     void TriggerAppearanceChange()
     {
-        // 调用之前的变身脚本
         if (appearanceScript != null)
         {
-            appearanceScript.ChangeSpriteRandomly(); // 需要把这个方法改为 public
+            appearanceScript.ChangeSpriteRandomly();
         }
     }
 }
